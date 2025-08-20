db310c712f157493224e8953dce559d8aaa10c28
Jose Luis <jlvh05@hotmail.com>
2025-08-20 16:07:52 -0700

    a

--- src/ml15/E00_rl/q_learning.py
+++ src/ml15/E00_rl/q_learning.py
@@ -11,7 +11,7 @@ class RandomAgent:
 
         # Tabla estados x acciones
         self.Q = np.zeros((env.observation_space.n, env.action_space.n))
-        # Parameters
+        # Parámetros
         self.alpha = alpha  # Learning rate
         self.gamma = gamma  # Discount factor
         self.epsilon = epsilon  # Exploration rate
@@ -29,48 +29,45 @@ class QLearningAgent(RandomAgent):
 
     def act(self, observation):
         if np.random.random() < self.epsilon:
-            return env.action_space.sample()  # Exploration
+            return self.action_space.sample()  # Exploración
         else:
-            return np.argmax(self.Q[observation])  # Exploitation
+            return np.argmax(self.Q[observation])  # Explotación
 
-    # Update Q values using Q-learning
     def step(self, state, action, reward, next_state):
         best_next_action = np.argmax(self.Q[next_state])
-        # TODO: Implementa la actualización de Q-learning usando la ecuación vista en clase
-        self.Q[state][action] = ...
+        td_target = reward + self.gamma * self.Q[next_state][best_next_action]
+        td_error = td_target - self.Q[state][action]
+        self.Q[state][action] += self.alpha * td_error
 
 
 if __name__ == "__main__":
-    # TODO:
-    # Este ejercicio cuenta como 5 pts extra en el primer examen parcial
-    # 1. completa el código para implementar q learning,
-    # 2. modifica los hiperparámetros para que el agente aprenda
-    # 3. ejecuta el script para ver el comportamiento del agente
-    # 4. Implementa una técnica para reducir la exploración conforme el agente aprende
-    # https://gymnasium.farama.org/environments/toy_text/cliff_walking/
     env = gym.make("CliffWalking-v1", render_mode="human")
 
     n_episodes = 1000
     episode_length = 200
-    agent = RandomAgent(env, alpha=0.1, gamma=0.9, epsilon=0.9)
+    agent = QLearningAgent(env, alpha=0.1, gamma=0.9, epsilon=1.0)
+
+    epsilon_decay = 0.995
+    min_epsilon = 0.05
+
     for e in range(n_episodes):
         obs, _ = env.reset()
         ep_return = 0
         for i in range(episode_length):
-            # take a random action
             action = agent.act(obs)
             next_obs, reward, done, _, _ = env.step(action)
-            # update agent
+
             agent.step(obs, action, reward, next_obs)
+
+            ep_return += reward
+            obs = next_obs
 
             if done:
                 break
-            ep_return += reward
-            obs = next_obs
-            print(agent.Q)
-            env.render()
-        # TODO: Implementa algun código para reducir la exploración del agente conforme aprende
-        # puedes decidir hacerlo por episodio, por paso del tiempo, retorno promedio, etc.
 
-        print(f"Episode {e} return: ", ep_return)
+        # Reducir exploración
+        agent.epsilon = max(min_epsilon, agent.epsilon * epsilon_decay)
+
+        print(f"Episode {e} return: {ep_return}, epsilon: {agent.epsilon:.3f}")
+
     env.close()
